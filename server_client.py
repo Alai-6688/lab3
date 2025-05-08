d024c3fed5ddadb5b6467a2b841823f6c94406c1
Alai <2879578952@qq.com>
2025-05-08 22:12:35 +0800

    commit

--- server_client.py
+++ server_client.py
@@ -18,3 +18,19 @@
 
    if __name__ == "__main__":
        start_server('localhost', 51234)
+
+   def handle_client(conn, addr):
+       while True:
+           data = conn.recv(1024).decode()
+           if not data:
+               break
+           cmd, key = data.split()
+           if cmd == 'R':
+               # Implement READ logic here and send response back to client
+               pass
+           elif cmd == 'G':
+               # Implement GET logic here and send response back to client
+               pass
+           elif cmd == 'P':
+               # Implement PUT logic here and send response back to client
+               pass
