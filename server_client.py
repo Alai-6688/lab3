22f387e23ac4ffd82ee260be51c27bdf1dca5078
Alai <2879578952@qq.com>
2025-05-08 22:28:16 +0800

    commit

--- server_client.py
+++ server_client.py
@@ -1,5 +1,5 @@
  import socket
-   import threading
+ import threading
 
    def handle_client(conn, addr):
        # Handle client requests here
@@ -34,3 +34,33 @@
            elif cmd == 'P':
                # Implement PUT logic here and send response back to client
                pass
+     
+     tuple_space = {}  # This will be the in-memory representation of the tuple space
+
+   def handle_client(conn, addr):
+       while True:
+           data = conn.recv(1024).decode()
+           if not data:
+               break
+           cmd, key, value = data.split()
+           if cmd == 'R':
+               if key in tuple_space:
+                   response = f"OK ({key}, {tuple_space[key]}) read"
+               else:
+                   response = "ERR k does not exist"
+               conn.send(response.encode())
+           elif cmd == 'G':
+               if key in tuple_space:
+                   response = f"OK ({key}, {tuple_space[key]}) removed"
+                   del tuple_space[key]
+               else:
+                   response = "ERR k does not exist"
+               conn.send(response.encode())
+           elif cmd == 'P':
+               if key in tuple_space:
+                   response = "ERR k already exists"
+               else:
+                   tuple_space[key] = value
+                   response = "OK (k, v) added"
+               conn.send(response.encode())
+    
